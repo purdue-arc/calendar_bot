51abdaacf52aa0651e8ba5bbc9ca3e4e80a7bfc8
Harrison McCarty <mccarth@purdue.edu>
2021-08-02 02:42:24 -0400

    fixed heroku imcompatibility with json

--- channel_watch.py
+++ channel_watch.py
@@ -1,8 +1,13 @@
 import discord
+import os
 import json
 
 client = discord.Client()
-discord_config = json.load(open('discord-config.json', 'r'))
+if 'DISCORD_CHANNELS' in os.environ:
+    active_channels = json.load(os.environ['DISCORD_CHANNELS'])
+else:
+    print("DISCORD_CHANNELS not set, using empty list.")
+    active_channels = []
 
 @client.event
 async def on_ready():
@@ -15,12 +20,15 @@ async def on_message(message):
 
     if message.content.startswith('$remindhere'):
         if "high council" in [r.name.lower() for r in message.author.roles]:
-            discord_config['channels'].append(message.channel.name)
-            json.dump(discord_config, open('discord-config.json', 'w'))
+            active_channels.append(message.channel.name)
+            os.environ['DISCORD_CHANNELS'] = json.dumps(active_channels)
             await message.channel.send('Sending reminders in this channel.')
 
 @client.event
 async def on_disconnect():
-    json.dump(discord_config, open('discord-config.json', 'w'))
+    os.environ['DISCORD_CHANNELS'] = json.dumps(active_channels)
 
-client.run(discord_config['token'])
+if 'DISCORD_TOKEN' in os.environ:
+    client.run(os.environ['DISCORD_TOKEN'])
+else:
+    print("DISCORD_TOKEN is not set.")
