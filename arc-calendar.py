7b27f559395e927ab3f2637d1ee29d7244753310
Harrison McCarty <mccarth@purdue>
2021-08-01 20:08:01 -0500

    working calendar requests

---
+++ arc-calendar.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2 import service_account
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SERVICE_ACCOUNT_FILE = 'service-account.json'
+ARC_CALENDAR = '8mmp47biqieqhn39slnf6rmi8s@group.calendar.google.com'
+
+
+def collect_today(n=5):
+    """
+    Gets n events within 24 hours of current time.
+    """
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    # delegated_creds = creds.with_subject(SUBJECT)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    start = datetime.datetime.utcnow()
+    end = start + datetime.timedelta(1)
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId=ARC_CALENDAR,
+                                          timeMin=start.isoformat() + 'Z',
+                                          timeMax=end.isoformat() + 'Z',
+                                          maxResults=n, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    if not events:
+        return None
+    return events
