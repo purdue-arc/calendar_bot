2a6beec726c3e250cf6bbc88a4c79a5c7e3ce560
Harrison McCarty <mccarth@purdue>
2021-08-06 16:00:45 -0400

    added a few commands

--- bot.py
+++ bot.py
@@ -6,11 +6,14 @@ import sys
 import os
 import discord
 import redis
+import my_calendar
+from send_updates import send_update
 from keep_alive import keep_alive
+
+ADMIN_ROLE = 'high council'
 
 client = discord.Client()
 r = redis.from_url(os.environ.get("REDIS_URL"))
-
 
 @client.event
 async def on_ready():
@@ -29,11 +32,26 @@ async def on_message(message):
     if message.content.startswith('.remindhere'):
         active_channels = r.smembers('DISCORD_CHANNELS')
         channel_exists = active_channels and message.channel.name not in active_channels
-        author_permitted = 'high council' in [
-            r.name.lower() for r in message.author.roles]
+        author_permitted = ADMIN_ROLE in [
+            role.name.lower() for role in message.author.roles]
         if not channel_exists and author_permitted:
             r.sadd('DISCORD_CHANNELS', message.channel.name)
             await message.channel.send('Sending reminders in this channel.')
+    elif message.content.startswith('.getupdates'):
+        author_permitted = ADMIN_ROLE in [
+            role.name.lower() for role in message.author.roles]
+        if author_permitted:
+            await send_update(message.channel, my_calendar.collect_today())
+    elif message.content.startswith('.getchannels'):
+        active_channels = r.smembers('DISCORD_CHANNELS')
+        author_permitted = ADMIN_ROLE in [
+            role.name.lower() for role in message.author.roles]
+        if author_permitted:
+            await message.channel.send('Sending reminders in channels; ' + str(active_channels))   
+    elif message.content.startswith('.clearchannels'):
+        r.flushall()
+        await message.channel.send('Cleared all channels.')
+
 
 if 'DISCORD_TOKEN' in os.environ:
     keep_alive()
